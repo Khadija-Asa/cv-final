3d1ec089c41a214342b301d76b61990e6e86665e
Khadija-Asa <khadija.aitsiali@gmail.com>
2021-12-03 16:47:45 +0100

    banner done with media queries

--- js/script.js
+++ js/script.js
@@ -18,3 +18,12 @@ function closeMenu() {
     burger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// SCROLL DOWN BUTTON
+
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
