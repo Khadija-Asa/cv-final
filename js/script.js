3ed3a0bfff0f9fb1d719dbc702704ab30708f741
Khadija-Asa <khadija.aitsiali@gmail.com>
2021-12-07 16:32:47 +0100

    scroll button removed

--- js/script.js
+++ js/script.js
@@ -21,9 +21,3 @@ function closeMenu() {
 
 // SCROLL DOWN BUTTON
 
-$(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
-  });
