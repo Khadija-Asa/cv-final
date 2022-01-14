8cff3bf182d95d40579d14d872075cd3c3d21817
Khadija-Asa <khadija.aitsiali@gmail.com>
2022-01-15 00:45:51 +0100

    problem with anim left resolved

--- js/script.js
+++ js/script.js
@@ -66,6 +66,6 @@ AOS.init({
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   delay: 400, // values from 0 to 3000, with step 50ms
-  duration: 1000, // values from 0 to 3000, with step 50ms
+  duration: 2000, // values from 0 to 3000, with step 50ms
 
 });
